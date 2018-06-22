eb8d5afdb009ff2003025bbc895f97a51cf79156
Mike Cardillo <mike@mjcardillo.com>
2018-06-21 19:46:06 -0700

    put main circles into an outer row

--- src/components/CircleButtons.jsx
+++ src/components/CircleButtons.jsx
@@ -4,23 +4,31 @@ import Circle from '../components/Circle';
 
 const CircleButtons = () => {
     return (
-        <div class="row container text-center" id="circle-wrapper">
-          <div class="col-sm-4">
-            <Link to="/experience">
-              <Circle circleText="Experience" faIcon="fa-briefcase" id="experience-btn"/>
-            </Link>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-2"/>
+          <div class="col-sm-8 container">
+            <div class="row text-center" id="circle-wrapper">
+              <div class="col-sm-4">
+                <Link class="nounderline" to="/experience">
+                  <Circle circleText="Experience" faIcon="fa-briefcase" id="experience-btn"/>
+                </Link>
+              </div>
+              <div class="col-sm-4">
+                <Link class="nounderline" to="/projects">
+                  <Circle circleText="Projects" faIcon="fa-cog" id="projects-btn"/>
+                </Link>
+              </div>
+              <div class="col-sm-4">
+                <a class="nounderline" href="https://github.com/mcardillo55">
+                  <Circle circleText="GitHub" faIcon="fa-github" id="github-btn"/>
+                </a>
+              </div>
+            </div>
           </div>
-          <div class="col-sm-4">
-            <Link to="/projects">
-              <Circle circleText="Projects" faIcon="fa-cog" id="projects-btn"/>
-            </Link>
-          </div>
-          <div class="col-sm-4">
-            <a href="https://github.com/mcardillo55">
-              <Circle circleText="GitHub" faIcon="fa-github" id="github-btn"/>
-            </a>
-          </div>
+          <div class="col-sm-2"/>
         </div>
+      </div>
     );
 }
 
