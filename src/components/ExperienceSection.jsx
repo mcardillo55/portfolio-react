7e18aa96f41e8fe28de308646683373f6413e1d4
Mike Cardillo <mcardillo55@gmail.com>
2019-02-05 15:36:50 -0800

    Added some images to the experience page

--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import CiscoLogo from '../images/cisco_logo.png';
+import PSULogo from '../images/psu_logo.png';
 
 const ExperienceSection = () => {
   return(
@@ -6,16 +8,23 @@ const ExperienceSection = () => {
         <div id="professional">
             <h2>Professional</h2>
             <div className="job">
-                <h3>Cisco Systems</h3>
-                <h5>San Jose, CA</h5>
-                <h4 className="job-title">Software Engineer - Kernel - September 2011 to September 2016</h4>
-                <ul className="job-roles">
-                    <li>Implemented prctl system calls to allow userspace code direct access to system page data, greatly reducing the amount of CPU context switching required for common tasks</li>
-                    <li>Automated quarterly merge and sync process of thousands of upstream Linux and FOSS patches into internal mainline kernel branch, reducing sync time from a week to 3 days</li>
-                    <li>Integrated Yocto embedded Linux environment into IOS-XE operating system, enabling platform independent support for many routing and switching platforms</li>
-                    <li>Enforced exclusive write or execute policy on all data segments, hardening systems against common overflow attacks</li>
-                    <li>Patched critical open source vulnerabilities with a short turnaround time</li>
-                </ul>
+                <div className="row">
+                    <div className="col-xs-2">
+                        <img className="img-responsive heading-image" src={CiscoLogo} alt="Cisco Logo"/>
+                    </div>
+                    <div className="col-xs-10">
+                        <h3>Cisco Systems</h3>
+                        <h5>San Jose, CA</h5>
+                    </div>
+                </div>
+                    <h4 className="job-title">Software Engineer - Kernel - September 2011 to September 2016</h4>
+                    <ul className="job-roles">
+                        <li>Implemented prctl system calls to allow userspace code direct access to system page data, greatly reducing the amount of CPU context switching required for common tasks</li>
+                        <li>Automated quarterly merge and sync process of thousands of upstream Linux and FOSS patches into internal mainline kernel branch, reducing sync time from a week to 3 days</li>
+                        <li>Integrated Yocto embedded Linux environment into IOS-XE operating system, enabling platform independent support for many routing and switching platforms</li>
+                        <li>Enforced exclusive write or execute policy on all data segments, hardening systems against common overflow attacks</li>
+                        <li>Patched critical open source vulnerabilities with a short turnaround time</li>
+                    </ul>
                 <h4 className="job-title">CDO Choice Intern - May 2010 to August 2010</h4>
                 <ul className="job-roles">
                     <li>Wrote several scripts in Perl to perform common source code management tasks, such as counting the total number of lines in a codebase, or determining which ClearCase component contained a given file</li>
@@ -25,8 +34,15 @@ const ExperienceSection = () => {
 
         <div id="education">
             <h2>Education</h2>
-            Bachelor of Science in Computer Engineering, June 2011<br/>
-            Pennsylvania State University<br/><br/>
+            <div className="row">
+                <div className="col-xs-2">
+                    <img className="img-responsive heading-image" src={PSULogo} alt="Penn State Logo"/>
+                </div>
+                <div className="col-xs-7">
+                    <h4>Pennsylvania State University</h4>
+                    <h5>Bachelor of Science in Computer Engineering, June 2011</h5>
+                </div>
+            </div>
         </div>
 
         <div id="skills">
