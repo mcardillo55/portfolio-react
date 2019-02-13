389412f9107814f1b8b30dd09ac64442245530fa
Mike Cardillo <mcardillo55@gmail.com>
2019-02-12 21:54:35 -0800

    Change col size from sm4 to md4 on circle buttons

--- src/components/CircleButtons.jsx
+++ src/components/CircleButtons.jsx
@@ -13,17 +13,17 @@ const CircleButtons = () => {
             <Container>
               <Col sm={{span:8, offset:2}}>
                 <Row id="circle-wrapper" className="text-center">
-                  <Col sm={4}>
+                  <Col md={4}>
                     <NavLink className="circle-link" activeClassName="circle-link-active" to="/experience">
                       <Circle circleText="Experience" faIcon="fa-briefcase" id="experience-btn"/>
                     </NavLink>
                   </Col>
-                  <Col sm={4}>
+                  <Col md={4}>
                     <NavLink className="circle-link" activeClassName="circle-link-active" to="/projects">
                       <Circle circleText="Projects" faIcon="fa-cog" id="projects-btn"/>
                     </NavLink>
                     </Col>
-                  <Col sm={4}>
+                  <Col md={4}>
                     <a className="circle-link" href="https://github.com/mcardillo55">
                       <Circle circleText="GitHub" faIcon="fa-github" id="github-btn"/>
                     </a>
