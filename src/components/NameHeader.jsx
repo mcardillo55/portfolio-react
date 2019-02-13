c08e230e96ff3abd620c77065f3b4ccdb9f8eebc
Mike Cardillo <mcardillo55@gmail.com>
2019-02-12 19:25:29 -0800

    Switch from page-header to bootstrap utility classes since page-header is removed from bootstrap 4

--- src/components/NameHeader.jsx
+++ src/components/NameHeader.jsx
@@ -5,7 +5,7 @@ import Col from 'react-bootstrap/Col';
 const NameHeader = () => {
     return(
       <Row>
-          <Col id="name" className="page-header text-center" md={12}>
+          <Col id="name" className="pb-2 mt-4 mb-2 border-bottom text-center" md={12}>
               <h1>Mike Cardillo</h1>
           </Col>
       </Row>
