bd6e694c37b686beb64743a47525a86742fd51e7
Mike Cardillo <mcardillo55@gmail.com>
2019-02-25 22:30:12 -0800

    Use bootstrap pills to display tech used

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Badge from 'react-bootstrap/Badge'
 
 const Project = (props) => {
     let title;
@@ -12,9 +13,13 @@ const Project = (props) => {
     }
     return(
         <div className="project">
-            <h6>{title}{title && " - "}{props.description} ({props.stack}) - {props.years}</h6>
+            <h6 className="mb-1">{title}{title && " - "}{props.description} - {props.years}</h6>
+            <div>
+                {props.stack.map(function(technology) {
+                    return <Badge pill className="mr-1 mb-1" variant="primary">{technology}</Badge>
+                })}
+            </div>
             <ul>
-
                 {props.tasks.map(function(task) {
                     return <li dangerouslySetInnerHTML={{__html: task}} />
                 })}
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -9,7 +9,7 @@ const ProjectsSection = () => {
             <Project title="cbpro-trader" 
                      link="https://github.com/mcardillo55/cbpro-trader"
                      description="Automated Cryptocurrency Trader"
-                     stack="Python"
+                     stack={["Python"]}
                      years="2017-2018" 
                      tasks={["Supports all cryptocurrencies available on Coinbase Pro",
                              "Collected real-time data from the Coinbase Pro cryptocurrency market, using their websocket API (via the <a href=\"https://github.com/danpaquin/coinbasepro-python\">coinbasepro-python</a> library)",
@@ -21,7 +21,7 @@ const ProjectsSection = () => {
             <Project title="soundcloud-dj" 
                      link="https://github.com/mcardillo55/soundcloud-dj"
                      description="YouTube/SoundCloud Hybrid Playlists"
-                     stack="Python/Flask/AngularJS"
+                     stack={["Python", "Flask", "AngularJS"]}
                      years="2014" 
                      tasks={["Used Facebook Graph API to search for SoundCloud and YouTube links in FB groups",
                              "Stored hybrid playlists of both YouTube and SoundCloud songs in an SQLite database",
@@ -29,13 +29,13 @@ const ProjectsSection = () => {
             <Project title="unfriended" 
                      link="https://github.com/mcardillo55/unfriended-flask"
                      description="Facebook Un-Friending Detector"
-                     stack="Python/Flask"
+                     stack={["Python", "Flask"]}
                      years="2014" 
                      tasks={["Used Facebook API to obtain user's friend data",
                              "Utilized Flask framework to store friend data in a MySQL database, which was then retrieved and compared with the user's current friends list on subsequent visits",
                              "Updated app to work around Facebook API restrictions that limited third-party access to friend's list data by parsing an uploaded version of the user's \"Friends\" page in HTML format"]}/>
             <Project description="Student Space Programs Laboratory"
-                     stack="C/GTK+"
+                     stack={["C", "GTK+"]}
                      years="2010" 
                      tasks={["Developed front end interface to parse XML messages from a near-space satellite, classify by severity and display in a table format on ground station computers"]}/>
         </div>
