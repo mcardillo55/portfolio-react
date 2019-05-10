530b9340a439e5c4ec2a05b86f920a6c3bad252d
Mike Cardillo <mike@mjcardillo.com>
2019-05-09 18:38:31 -0700

    Remove ListGroup from Card Body (to remove extra padding)

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -18,16 +18,16 @@ const ProjectCard = (props) => {
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <div>
-                {props.stack.map(function(technology) {
-                    return <Badge pill className="mr-1" variant="primary">{technology}</Badge>
-                })}
-            </div>
+                    {props.stack.map(function(technology) {
+                        return <Badge pill className="mr-1" variant="primary">{technology}</Badge>
+                    })}
+                </div>
+            </Card.Body>
             <ListGroup variant="flush">
                 {props.tasks.map(function(task) {
                     return <ListGroup.Item dangerouslySetInnerHTML={{__html: task}} />
                 })}
             </ListGroup>
-            </Card.Body>
         </Card>
     )
 };
