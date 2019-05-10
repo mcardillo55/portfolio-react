6edf6fb07045e22c5e32f734a2bcc02e5c8c894f
Mike Cardillo <mike@mjcardillo.com>
2019-05-09 18:34:38 -0700

    Switched Projects section to cards

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Badge from 'react-bootstrap/Badge'
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+
+const ProjectCard = (props) => {
+    let title;
+    if (props.title) {
+            if (props.link) {
+                title = <a href={props.link}>{props.title}</a>;
+            } else {
+                title = props.title
+            }
+                    
+    }
+    return(
+        <Card>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <div>
+                {props.stack.map(function(technology) {
+                    return <Badge pill className="mr-1" variant="primary">{technology}</Badge>
+                })}
+            </div>
+            <ListGroup variant="flush">
+                {props.tasks.map(function(task) {
+                    return <ListGroup.Item dangerouslySetInnerHTML={{__html: task}} />
+                })}
+            </ListGroup>
+            </Card.Body>
+        </Card>
+    )
+};
+
+export default ProjectCard;
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,43 +1,55 @@
 import React from 'react';
-import Project from './Project'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import ProjectCard from './ProjectCard'
 
 const ProjectsSection = () => {
   return(
     <div id="projects">
         <div id="projects">
             <h3>Projects</h3>
-            <Project title="cbpro-trader" 
-                     link="https://github.com/mcardillo55/cbpro-trader"
-                     description="Automated Cryptocurrency Trader"
-                     stack={["Python"]}
-                     years="2017-2018" 
-                     tasks={["Supports all cryptocurrencies available on Coinbase Pro",
-                             "Collected real-time data from the Coinbase Pro cryptocurrency market, using their websocket API (via the <a href=\"https://github.com/danpaquin/coinbasepro-python\">coinbasepro-python</a> library)",
-                             "Performed real-time technical analysis of cryptocurrency prices, to generate common indicators used in currency trading (using <a href=\"https://github.com/mrjbq7/ta-lib\">ta-lib library</a>)",
-                             "Placed buy and sell orders, in Python threads, depending on technical indicators",
-                             "Determined, based on current market volatility, whether to use free limit orders or instant market orders",
-                             "Created curses interface to view real-time program data in console",
-                             "Made contributions back to gdax-python (<a href='https://github.com/danpaquin/coinbasepro-python'>https://github.com/danpaquin/coinbasepro-python</a>)"]}/>
-            <Project title="soundcloud-dj" 
-                     link="https://github.com/mcardillo55/soundcloud-dj"
-                     description="YouTube/SoundCloud Hybrid Playlists"
-                     stack={["Python", "Flask", "AngularJS"]}
-                     years="2014" 
-                     tasks={["Used Facebook Graph API to search for SoundCloud and YouTube links in FB groups",
-                             "Stored hybrid playlists of both YouTube and SoundCloud songs in an SQLite database",
-                             "Utilized AngularJS and YouTube/SoundCloud JavaScript Player APIs to dynamically play songs in their corresponding player, based on the URL"]}/>
-            <Project title="unfriended" 
-                     link="https://github.com/mcardillo55/unfriended-flask"
-                     description="Facebook Un-Friending Detector"
-                     stack={["Python", "Flask"]}
-                     years="2014" 
-                     tasks={["Used Facebook API to obtain user's friend data",
-                             "Utilized Flask framework to store friend data in a MySQL database, which was then retrieved and compared with the user's current friends list on subsequent visits",
-                             "Updated app to work around Facebook API restrictions that limited third-party access to friend's list data by parsing an uploaded version of the user's \"Friends\" page in HTML format"]}/>
-            <Project description="Student Space Programs Laboratory"
-                     stack={["C", "GTK+"]}
-                     years="2010" 
-                     tasks={["Developed front end interface to parse XML messages from a near-space satellite, classify by severity and display in a table format on ground station computers"]}/>
+            <Row>
+              <Col md={3}>
+                <ProjectCard title="cbpro-trader" 
+                        link="https://github.com/mcardillo55/cbpro-trader"
+                        description="Automated Cryptocurrency Trader"
+                        stack={["Python"]}
+                        years="2017-2018" 
+                        tasks={["Supports all cryptocurrencies available on Coinbase Pro",
+                                "Collected real-time data from the Coinbase Pro cryptocurrency market, using their websocket API (via the <a href=\"https://github.com/danpaquin/coinbasepro-python\">coinbasepro-python</a> library)",
+                                "Performed real-time technical analysis of cryptocurrency prices, to generate common indicators used in currency trading (using <a href=\"https://github.com/mrjbq7/ta-lib\">ta-lib library</a>)",
+                                "Placed buy and sell orders, in Python threads, depending on technical indicators",
+                                "Determined, based on current market volatility, whether to use free limit orders or instant market orders",
+                                "Created curses interface to view real-time program data in console",
+                                "Made contributions back to gdax-python (<a href='https://github.com/danpaquin/coinbasepro-python'>https://github.com/danpaquin/coinbasepro-python</a>)"]}/>
+              </Col>
+              <Col md={3}>
+                <ProjectCard title="soundcloud-dj" 
+                        link="https://github.com/mcardillo55/soundcloud-dj"
+                        description="YouTube/SoundCloud Hybrid Playlists"
+                        stack={["Python", "Flask", "AngularJS"]}
+                        years="2014" 
+                        tasks={["Used Facebook Graph API to search for SoundCloud and YouTube links in FB groups",
+                                "Stored hybrid playlists of both YouTube and SoundCloud songs in an SQLite database",
+                                "Utilized AngularJS and YouTube/SoundCloud JavaScript Player APIs to dynamically play songs in their corresponding player, based on the URL"]}/>
+              </Col>
+              <Col md={3}>
+                <ProjectCard title="unfriended" 
+                        link="https://github.com/mcardillo55/unfriended-flask"
+                        description="Facebook Un-Friending Detector"
+                        stack={["Python", "Flask"]}
+                        years="2014" 
+                        tasks={["Used Facebook API to obtain user's friend data",
+                                "Utilized Flask framework to store friend data in a MySQL database, which was then retrieved and compared with the user's current friends list on subsequent visits",
+                                "Updated app to work around Facebook API restrictions that limited third-party access to friend's list data by parsing an uploaded version of the user's \"Friends\" page in HTML format"]}/>
+              </Col>
+              <Col md={3}>
+                <ProjectCard title="Student Space Programs Laboratory"
+                        stack={["C", "GTK+"]}
+                        years="2010" 
+                        tasks={["Developed front end interface to parse XML messages from a near-space satellite, classify by severity and display in a table format on ground station computers"]}/>
+              </Col>
+            </Row>
         </div>
     </div>
   )
