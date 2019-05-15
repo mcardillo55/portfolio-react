5066d8774340b196779fdd23c3c2ab2fe528afcc
Mike Cardillo <mike@mjcardillo.com>
2019-05-14 19:03:29 -0700

    Changed bullet points for Project to sound less like resume

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -8,46 +8,51 @@ const ProjectsSection = () => {
     <div id="projects">
         <div id="projects">
             <h3>Projects</h3>
-            <Row>
-              <Col md={3}>
+            <Row className="mb-3">
+              <Col md={6}>
                 <ProjectCard title="cbpro-trader" 
-                        link="https://github.com/mcardillo55/cbpro-trader"
+                        source_link="https://github.com/mcardillo55/cbpro-trader"
                         description="Automated Cryptocurrency Trader"
                         stack={["Python"]}
                         years="2017-2018" 
-                        tasks={["Supports all cryptocurrencies available on Coinbase Pro",
-                                "Collected real-time data from the Coinbase Pro cryptocurrency market, using their websocket API (via the <a href=\"https://github.com/danpaquin/coinbasepro-python\">coinbasepro-python</a> library)",
-                                "Performed real-time technical analysis of cryptocurrency prices, to generate common indicators used in currency trading (using <a href=\"https://github.com/mrjbq7/ta-lib\">ta-lib library</a>)",
-                                "Placed buy and sell orders, in Python threads, depending on technical indicators",
-                                "Determined, based on current market volatility, whether to use free limit orders or instant market orders",
-                                "Created curses interface to view real-time program data in console",
-                                "Made contributions back to gdax-python (<a href='https://github.com/danpaquin/coinbasepro-python'>https://github.com/danpaquin/coinbasepro-python</a>)"]}/>
+                        tasks={["Consumes tick data <a href='https://pro.coinbase.com/'>Coinbase Pro</a> websocket",
+                                "Calculates <a href='https://en.wikipedia.org/wiki/Technical_analysis'>technical analysis</a> indicators",
+                                "Places buy and sell orders in threads via the Coinbase Pro REST API",
+                                "Determine, based on market volatility, whether to place free limit orders or instant market orders",
+                                "Features curses interface to view real-time program data in console"]}/>
               </Col>
-              <Col md={3}>
+              <Col md={6}>
                 <ProjectCard title="soundcloud-dj" 
-                        link="https://github.com/mcardillo55/soundcloud-dj"
+                        source_link="https://github.com/mcardillo55/soundcloud-dj"
+                        demo_link="https://www.shipfamradio.com/"
                         description="YouTube/SoundCloud Hybrid Playlists"
                         stack={["Python", "Flask", "AngularJS"]}
                         years="2014" 
-                        tasks={["Used Facebook Graph API to search for SoundCloud and YouTube links in FB groups",
-                                "Stored hybrid playlists of both YouTube and SoundCloud songs in an SQLite database",
-                                "Utilized AngularJS and YouTube/SoundCloud JavaScript Player APIs to dynamically play songs in their corresponding player, based on the URL"]}/>
+                        tasks={["Searched for SoundCloud and YouTube links in Facebook groups using Graph API",
+                                "Graph API functionality was deprecated. Instead uses hourly cron job to run a PhantomJS script and graph the group data",
+                                "Song data is submitted via POST request to Flask backend and stored in a SQLite database",
+                                "Used AngularJS and YouTube/SoundCloud Frontend APIs to play songs in their corresponding player",
+                                "Playlist is updated live, via Socket.IO, if page is open while songs are added to backend"]}/>
               </Col>
-              <Col md={3}>
+            </Row>
+            <Row>
+              <Col md={6}>
                 <ProjectCard title="unfriended" 
-                        link="https://github.com/mcardillo55/unfriended-flask"
+                        source_link="https://github.com/mcardillo55/unfriended-flask"
+                        demo_link="https://www.michaeljcardillo.com/unfriended-flask/"
                         description="Facebook Un-Friending Detector"
                         stack={["Python", "Flask"]}
                         years="2014" 
-                        tasks={["Used Facebook API to obtain user's friend data",
+                        tasks={["Grabs user's friend list via Facebook's Graph API (deprecated: now uses user submitted HTML)",
                                 "Utilized Flask framework to store friend data in a MySQL database, which was then retrieved and compared with the user's current friends list on subsequent visits",
                                 "Updated app to work around Facebook API restrictions that limited third-party access to friend's list data by parsing an uploaded version of the user's \"Friends\" page in HTML format"]}/>
               </Col>
-              <Col md={3}>
+              <Col md={6}>
                 <ProjectCard title="Student Space Programs Laboratory"
                         stack={["C", "GTK+"]}
                         years="2010" 
-                        tasks={["Developed front end interface to parse XML messages from a near-space satellite, classify by severity and display in a table format on ground station computers"]}/>
+                        tasks={["Developed front end interface to parse XML messages from a near-space satellite",
+                                "Messages were classified by severity and displayed in table"]}/>
               </Col>
             </Row>
         </div>
