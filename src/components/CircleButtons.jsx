88df40cfbc46d5a08c1928339fea6e454ed747da
Mike Cardillo <mike@mjcardillo.com>
2019-05-24 11:44:40 -0700

    Made Nav buttons look better on smaller screens

--- src/components/CircleButtons.jsx
+++ src/components/CircleButtons.jsx
@@ -10,27 +10,27 @@ const CircleButtons = (props) => {
     return (
       <Container>
         <Row>
-          <Col sm={{span: 7, offset:2}} md={{span:10, offset: 1}}>
+          <Col sm={{span: 8, offset:2}} md={{span:10, offset: 1}}>
             <Row id="circle-wrapper" className="text-center">
-              <Col xs="12" md="3">
+              <Col md={3}>
                 <Link className="circle-link" href="#" to="projects" spy={true} smooth={true} hashSpy={true} duration={500} onClick={props.onClick}>
                   <Circle circleText="Projects" faIcon="fa-cog" id="projects-btn"/>
                   <SmallNav faIcon="fa-cog" text="Projects"/>
                 </Link>
               </Col>
-              <Col xs="12" md="3">
+              <Col md={3}>
                 <Link className="circle-link" href="#" to="experience" spy={true} smooth={true} hashSpy={true} duration={500} onClick={props.onClick}>
                   <Circle circleText="Experience" faIcon="fa-briefcase" id="experience-btn"/>
                   <SmallNav faIcon="fa-briefcase" text="Experience"/>
                 </Link>
               </Col>
-              <Col xs="12" md="3">
+              <Col md={3}>
                 <a className="circle-link" href="/mike_cardillo_resume.pdf">
                   <Circle circleText="Resume" faIcon="fa-file-text" id="experience-btn"/>
                   <SmallNav faIcon="fa-file-text" text="Resume"/>
                 </a>
               </Col>
-              <Col xs="12" md="3">
+              <Col md={3}>
                 <a className="circle-link" href="https://github.com/mcardillo55">
                   <Circle circleText="GitHub" faIcon="fa-github" id="github-btn"/>
                   <SmallNav faIcon="fa-github" text="GitHub"/>
--- src/components/SmallNav.jsx
+++ src/components/SmallNav.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SmallNav = (props) => {
     return (
-        <div className="small-nav d-md-none">
+        <div className="small-nav d-md-none rounded my-1">
             <i className={"fa " + props.faIcon}/> {props.text}
         </div>
     );
