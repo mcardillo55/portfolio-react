7f08466bc5db8288bf13054118a4698af2d49e44
Mike Cardillo <mike@mjcardillo.com>
2019-05-23 19:57:44 -0700

    Add resume circle

--- src/components/CircleButtons.jsx
+++ src/components/CircleButtons.jsx
@@ -12,19 +12,23 @@ const CircleButtons = (props) => {
         <Row>
           <Col sm={{span: 7, offset:2}} md={{span:8, offset: 2}}>
             <Row id="circle-wrapper" className="text-center">
-              <Col xs="12" md="4">
-                <Link className="circle-link" to="projects" spy={true} smooth={true} hashSpy={true} duration={500} onClick={props.onClick}>
+              <Col xs="12" md="3">
                   <Circle circleText="Projects" faIcon="fa-cog" id="projects-btn"/>
                   <SmallNav faIcon="fa-cog" text="Projects"/>
                 </Link>
               </Col>
-              <Col xs="12" md="4">
-                <Link className="circle-link" to="experience" spy={true} smooth={true} hashSpy={true} duration={500} onClick={props.onClick}>
+              <Col xs="12" md="3">
                   <Circle circleText="Experience" faIcon="fa-briefcase" id="experience-btn"/>
                   <SmallNav faIcon="fa-briefcase" text="Experience"/>
                 </Link>
               </Col>
-              <Col xs="12" md="4">
+              <Col xs="12" md="3">
+                <a className="circle-link" href="/mike_cardillo_resume.pdf">
+                  <Circle circleText="Resume" faIcon="fa-file-text" id="experience-btn"/>
+                  <SmallNav faIcon="fa-file-text" text="Resume"/>
+                </a>
+              </Col>
+              <Col xs="12" md="3">
                 <a className="circle-link" href="https://github.com/mcardillo55">
                   <Circle circleText="GitHub" faIcon="fa-github" id="github-btn"/>
                   <SmallNav faIcon="fa-github" text="GitHub"/>
